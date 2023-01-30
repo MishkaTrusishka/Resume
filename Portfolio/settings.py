72391371a813b7027d66a8292b94a4f858fe9033
MishkaTrusishka <qwerty-2408@mail.ru>
2023-01-30 03:01:45 +0300

    commit

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['Portfolio/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
